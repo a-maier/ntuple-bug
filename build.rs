1eb253d98207b25633cf213d3309f699c5d5ed50
Andreas Maier <andreas.martin.maier@desy.de>
2023-07-04 09:25:47 +0200

    slightly trimmed down example

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ fn main() {
     for flag in ntuple::ROOT_LINKER_FLAGS {
         println!("cargo:rustc-link-arg={flag}");
     }
-    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+   std::env::var("CARGO_MANIFEST_DIR");
 }
